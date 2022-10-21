c88fcdb05bb76bd38ffac25e39f9da36e83fea7a
JoseXS <xskunk@gmail.com>
2022-10-21 18:37:21 +0200

    feat: Se añade script & pm2

--- scripts/scraping_get_artists.py
+++ scripts/scraping_get_artists.py
@@ -19,51 +19,58 @@ def init():
 def generate_items(data):
     try:
         artists = []
-        name_ok = ''
-        styles_ok = ''
-        country_ok = ''
+        styles_list = []
+        name = ''
+        styles = ''
+        country = ''
         for item in data:
-            if (name_ok != '' and styles_ok != '' and country_ok != ''):
-                artist = Artist(name_ok, country_ok, styles_ok)
+            if (name != '' and styles != '' and country != ''):
+                artist = Artist(name, country, styles)
                 artists.append(artist)
-                name_ok = ''
-                styles_ok = ''
-                country_ok = ''
+                name = ''
+                styles = ''
+                country = ''
             dataset = set_name_country_styles(item)
             if dataset[0] != "":
-                name_ok = dataset[0]
+                name = dataset[0]
             if dataset[1] != "":
-                country_ok = dataset[1]
+                country = dataset[1]
             if dataset[2] != "":
-                styles_ok = dataset[2]
-
-        set_wikipedia_atributes(artists)
+                styles = dataset[2]
+                if len(styles) > 0 and styles[0] != '' and styles[0] != 'Genre':
+                    for style_i in styles:
+                        styles_list.append(style_i)
+        # set_wikipedia_atributes(artists)
+        
+        styles_list = set(styles_list)
+        print(styles_list)
         
     except ValueError:
         print("❌ Error al obtener Artistas")
 
-
 def set_name_country_styles(item):
-    name_ok = ''
-    styles_ok = ''
-    country_ok = ''
-    name = item.find('a', class_="ajax_link cap")
-    style = item.find('td', class_="last")
-    if (name):
-        name_ok = name.text.title()
-    if (style):
-        style.text.split(', ')
-        styles_ok = style.text.split(', ')
+    try:
+        name_ok = ''
+        styles_ok = ''
+        country_ok = ''
+        name = item.find('a', class_="ajax_link cap")
+        style = item.find('td', class_="last")
+        if (name):
+            name_ok = name.text.title()
+        if (style):
+            style.text.split(', ')
+            styles_ok = style.text.split(', ')
 
-    country_imgs = item.select('img[src*="flags"]')
-    if len(country_imgs) > 0:
-        country_img = country_imgs[0]
-        country_a = str(country_img).split('src="/tpls/img/flags/16/')[1]
-        country_b = country_a.split(
-            '.png" style="float:left;margin-right:5px;"/>')
-        country_ok = country_b[0]
-
-    return [name_ok, country_ok, styles_ok]
+        country_imgs = item.select('img[src*="flags"]')
+        if len(country_imgs) > 0:
+            country_img = country_imgs[0]
+            country_a = str(country_img).split('src="/tpls/img/flags/16/')[1]
+            country_b = country_a.split(
+                '.png" style="float:left;margin-right:5px;"/>')
+            country_ok = country_b[0]
+        return [name_ok, country_ok, styles_ok]
+    except ValueError:
+        print("❌ Error al setear NCS")
 
 
 def slugify(s):
---
+++ scripts/update.sh
@@ -0,0 +1,51 @@
+source $(pwd)/scripts/setenv.sh
+source $(pwd)/scripts/utils/utils.sh
+exec 2>$(pwd)/scripts/error.log
+
+clear
+if [ "${NODE_ENV}" = "development" ]; then
+    logo
+fi
+
+echo "🔥  Actualizando backend de produccion"
+
+if [ "${NODE_ENV}" = "development" ]; then
+    COMMAND="cd /home/josexs/apps/xsmusic && npm run update"
+    COMMANDS="bash -i -c '${COMMAND}'"
+  
+    echo "🔥  Actualizando desde local"
+    ssh ${SSH_HOST} ${COMMANDS} >/dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo "✅  Actualizacion de backend desde local finalizada"
+    else
+        echo "❌  Actualizacion de backend desde local fallida"
+        exit 1
+    fi
+else
+    pm2 stop xsmusic >/dev/null 2>&1
+    PATH_BACKEND="/home/josexs/apps/xsmusic"
+    echo "🔥  Actualizando backend desde Produccion"
+    cd /home/josexs/apps/xsmusic && git checkout . && git pull >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "✅  Pull finalizado"
+    else
+        echo "❌  Pull fallido"
+        exit 1
+    fi
+    cd $PATH_BACKEND && npm i >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "✅  Instalacion de dependencias finalizada"
+    else
+        echo "❌  Instalacion de dependencias fallida"
+        exit 1
+    fi
+
+    pm2 restart xsmusic >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "✅  Pm2 finalizado"
+    else
+        echo "❌  Pm2 fallido"
+        exit 1
+    fi
+fi
