8542c26fb114df3422322d64de5d26ac99756caf
JoseXS <xskunk@gmail.com>
2022-10-21 21:02:11 +0200

    fix: Se añade script

---
+++ scripts/pwa.sh
@@ -0,0 +1,29 @@
+source $(pwd)/scripts/setenv.sh
+
+path="${PATH_PROJECT}/app"
+
+generate() {
+    cd ${path} && npm run build > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "✅  Build finalizado"
+    else
+        echo "❌  Creacion fallida"
+        exit 1
+    fi
+}
+
+deploy() {
+    cd ${path} && firebase deploy --project xsmusic-69 > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "✅  Deploy finalizado"
+        echo "💎  Ruta APP: https://xsmusic.carstournaments.com"
+    else
+        echo "❌  Deploy fallido"
+        exit 1
+    fi
+}
+
+clear
+echo "🔨  Generando PWA"
+generate
+deploy
