0766ea5c72ea7bab55c1669bf435ad76f2bc8497
JoseXS <xskunk@gmail.com>
2023-01-01 22:32:40 +0100

    fix: Correcciones en resume

--- src/modules/resume/resume.service.ts
+++ src/modules/resume/resume.service.ts
@@ -15,36 +15,42 @@ export class ResumeService {
         page: 1,
         pageSize: 20,
         hiddenSocial: true,
+        order: ['created', 'desc'],
       });
       const events = await this.eventService.getAll({
         page: 1,
         pageSize: 8,
         old: false,
         hiddenSocial: true,
+        order: ['date', 'asc'],
       });
       const sets = await this.mediaService.getAll({
         page: 1,
         pageSize: 4,
         type: 'set',
         hiddenSocial: true,
+        order: ['created', 'desc'],
       });
       const tracks = await this.mediaService.getAll({
         page: 1,
         pageSize: 4,
         type: 'track',
         hiddenSocial: true,
+        order: ['created', 'desc'],
       });
       const clubs = await this.siteService.getAll({
         page: 1,
         pageSize: 15,
         type: 'club',
         hiddenSocial: true,
+        order: ['created', 'desc'],
       });
       const festivals = await this.siteService.getAll({
         page: 1,
         pageSize: 15,
         type: 'festival',
         hiddenSocial: true,
+        order: ['created', 'desc'],
       });
       return { artists, events, sets, tracks, clubs, festivals };
     } catch (error) {
